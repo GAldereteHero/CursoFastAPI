0a0d489e1c943e18ad87d209169ac784aecd77bb
GAldereteHero <herogonzalo@gmail.com>
2022-07-06 15:08:22 -0300

    Query parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from operator import gt
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
@@ -8,9 +9,21 @@ inventory = {
         'name': "Leche",
         'precio': '150',
         'marca': 'serenisima'
+    },
+    2:{
+        'name': "YerbaMate",
+        'precio': '350',
+        'marca': 'Taragui'
     }
 }
 
 @app.get('/getInventory/{item_id}')
-def getItem( item_id : int):
+def getItem( item_id : int = Path(None, description="El ID del producto que deseamos seleccionar", gt=0,lt=2)):
     return inventory[item_id]
+
+@app.get('/getByName')
+def getItem( name : str):
+    for itemID in inventory:
+        if inventory[itemID]['name'] == name:
+            return inventory[itemID]
+    return {"Data":'No existe'}
