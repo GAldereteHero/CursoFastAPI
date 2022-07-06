c9e4e3f3df53fa96f2d45fc38f0215237bd2d7f7
GAldereteHero <herogonzalo@gmail.com>
2022-07-06 15:22:22 -0300

    Query parameters y path parameters

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from operator import gt
 from fastapi import FastAPI, Path
+from typing import Optional
 
 app = FastAPI()
 
@@ -21,8 +22,8 @@ inventory = {
 def getItem( item_id : int = Path(None, description="El ID del producto que deseamos seleccionar", gt=0,lt=2)):
     return inventory[item_id]
 
-@app.get('/getByName')
-def getItem( name : str):
+@app.get('/getByName/{item_id}')
+def getItem(*, name : Optional[str], tesy = int):
     for itemID in inventory:
         if inventory[itemID]['name'] == name:
             return inventory[itemID]
